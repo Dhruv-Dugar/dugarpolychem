562e6da5f345a6673732e2adf14d1c1fb4e0bcac
Dhurv Dugar <dhruvdugar02@gmail.com>
2024-01-30 20:53:20 +0530

    removed data.json

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-import { useState, useEffect } from "react";
-import { Navigation } from "./components/Navigation"
-import { Header } from "./components/header";
-import { About } from "./components/about.jsx"; 
-import { Services } from "./components/services.jsx"; 
-import { Gallery } from "./components/gallery.jsx"; 
-import {Contact} from "./components/contact.jsx";
-import JsonData from "./data/data.json"; 
-import SmoothScroll from "smooth-scroll"; 
+import NavigationBar from "./components/Navigation"
+import Header from "./components/header.jsx";
+import About from "./components/about.jsx";
+import Services from "./components/services.jsx";
+import Gallery from "./components/gallery.jsx";
+import { Contact } from "./components/contact.jsx";
+import SmoothScroll from "smooth-scroll";
 import "./App.css";
 
 export const scroll = new SmoothScroll('a[href*="#"]', {
@@ -14,22 +12,17 @@ export const scroll = new SmoothScroll('a[href*="#"]', {
   speedAsDuration: true,
 });
 
-const App = () => {
-  const [landingPageData, setLandingPageData] = useState({});
-  useEffect(() => {
-    setLandingPageData(JsonData);
-  }, []);
-
+function App() {
   return (
     <div>
-      <Navigation />
-      <Header data={landingPageData.Header} />
-      <About data={landingPageData.About} />
-      <Services data={landingPageData.Services} />
-      <Gallery data={landingPageData.Gallery}/>
-      <Contact data={landingPageData.Contact} />
+      <NavigationBar />
+      <Header />
+      <About />
+      <Services />
+      <Gallery />
+      <Contact />
     </div>
-  );
-};
+  )
+}
 
 export default App;
